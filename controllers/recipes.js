f05009a9671e2187e2bd2f9af6ee8f41e768c280
Zeynp62 <zainab.fadhel.alaali62@gmail.com>
2024-12-05 16:13:02 +0300

    still trying

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -8,20 +8,23 @@ const Ingredient = require('../models/ingredient.js')
 router.get('/', async (req, res) => {
   res.render('recipes/index.ejs')
 })
-router.post('/', async (req, res) => {
-  req.body.owner = req.session.user._id
-  await Recipe.create(req.body)
-  res.redirect('/recipes')
-})
+// router.post('/', async (req, res) => {
+//   req.body.owner = req.session.user._id
+//   await Recipe.create(req.body)
+//   res.redirect('/recipes')
+// })
 router.get('/new', async (req, res) => {
   const ingredient = await Ingredient.find()
   res.render('recipes/new.ejs', { ingredient })
 })
 router.post('/', async (req, res) => {
   try {
+    console.log('session', req.session)
+    console.log('user', req.session.user)
     const newRecipe = new Recipe(req.body)
     newRecipe.owner = req.session.user._id
     await newRecipe.save()
+    res.redirect('/recipes')
   } catch (error) {
     console.error(error)
     res.redirect('/')
